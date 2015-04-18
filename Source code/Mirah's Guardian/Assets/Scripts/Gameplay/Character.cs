a787368d044330f1a15188c3870f997980106dba
Ivan Schuaste Cavalheiro <ivanscavalheiro@hotmail.com>
2015-04-18 13:41:28 -0300

    Reorganizando projeto
    criado manager para interface
    reogarnizado pasta de scripts
    criado prefab com interface para ser usado em outras cenas de jogo

--- Source code/Mirah's Guardian/Assets/Scripts/Gameplay/Character.cs
+++ Source code/Mirah's Guardian/Assets/Scripts/Gameplay/Character.cs
@@ -1,31 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Character : MonoBehaviour {
 
 	//Variaveis
 
 	public int HP = 3;
-	public float vel = 1; //Velocidade do player
+	public float speed = 1; //Velocidade do player
 	public bool pausado = false;
 	public CanvasGroup MenuJogo;//Usado para fazer as interaçoes dos menus
 	public CanvasGroup MenuPausa;//-----/\
 
-	void Start () {
+	void Start () 
+	{
 	
 	}
 	
-	void Update () {
-		//Movimentaçao----------------------------------
-		if(Input.GetKey("w"))
-			transform.Translate(0,vel*Time.deltaTime,0);
-		if(Input.GetKey("s"))
-			transform.Translate(0,-vel*Time.deltaTime,0);
-		if(Input.GetKey("a"))
-			transform.Translate(-vel*Time.deltaTime,0,0);
-		if(Input.GetKey("d"))
-			transform.Translate(vel*Time.deltaTime,0,0);
-		//-----------------------------------------------
+	void Update () 
+	{
 		//Abre menu de pausa-------------------------------
 		if(Input.GetKeyDown("escape"))
 		{
---
+++ Source code/Mirah's Guardian/Assets/Scripts/Interface/InterfaceManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InterfaceManager : MonoBehaviour 
+{
+	public bool paused = false;
+	public CanvasGroup gameInterface;
+	public CanvasGroup pauseInterface;
+
+	void Update()
+	{
+		if(Input.GetKeyDown("escape"))
+			OnEscapePressed();
+	}
+
+	public void OnEscapePressed()
+	{
+		paused = !paused;
+		
+		Time.timeScale = (paused) ? 0 : 1 ;
+		EnableCanvas(gameInterface, !paused);
+		EnableCanvas(pauseInterface, paused);
+	}
+
+	private void EnableCanvas(CanvasGroup p_canvas, bool p_enable)
+	{
+		p_canvas.alpha = (p_enable) ? 1 : 0;
+		p_canvas.blocksRaycasts = p_enable;
+		p_canvas.interactable = p_enable;
+	}
+
+}
