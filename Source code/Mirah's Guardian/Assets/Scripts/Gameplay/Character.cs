e9f1bfb22dcf0bfff6391f0d5b6af9209d52a980
Ivan Schuaste Cavalheiro <ivanscavalheiro@hotmail.com>
2015-04-18 13:46:39 -0300

    Fix character script
    added mapmanager script
    fixed character script

--- Source code/Mirah's Guardian/Assets/Scripts/Gameplay/Character.cs
+++ Source code/Mirah's Guardian/Assets/Scripts/Gameplay/Character.cs
@@ -1,15 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Character : MonoBehaviour {
-
-	//Variaveis
-
-	public int HP = 3;
-	public float speed = 1; //Velocidade do player
-	public bool pausado = false;
-	public CanvasGroup MenuJogo;//Usado para fazer as interaçoes dos menus
-	public CanvasGroup MenuPausa;//-----/\
+public class Character : MonoBehaviour 
+{
+	public float helthPoints = 3;
+	public float speed = 1;
 
 	void Start () 
 	{
@@ -18,38 +13,6 @@ public class Character : MonoBehaviour {
 	
 	void Update () 
 	{
-		//Abre menu de pausa-------------------------------
-		if(Input.GetKeyDown("escape"))
-		{
-			switch(pausado)
-			{
-				case false://Se o jogo nao estiver pausado, agora ele esta
-					MenuJogo.alpha = 0;
-					MenuPausa.alpha = 1;
-					MenuPausa.blocksRaycasts = true;
-					MenuPausa.interactable = true;
-					Time.timeScale = 0; //Pausa o jogo
-					pausado = true;
-					break;
-				case true: //Contrario de cima
-					MenuJogo.alpha = 1;
-					MenuPausa.alpha = 0;
-					MenuPausa.blocksRaycasts = false;
-					MenuPausa.interactable = false;
-					Time.timeScale = 1;
-					pausado = false;
-					break;
-			}
-		}
-		//-----------------------------------------------
-	}
 
-	public void CliqueMenu () { //Usada quando se clica no botao RetornarJogo
-		MenuJogo.alpha = 1;
-		MenuPausa.alpha = 0;
-		MenuPausa.blocksRaycasts = false;
-		MenuPausa.interactable = false;
-		Time.timeScale = 1;
-		pausado = false;
 	}
 }
---
+++ Source code/Mirah's Guardian/Assets/Scripts/Gameplay/MapManager.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapManager : MonoBehaviour 
+{
+	public Map map;
+}
