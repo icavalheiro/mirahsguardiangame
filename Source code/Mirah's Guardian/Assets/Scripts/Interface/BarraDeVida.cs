9550037f56dcf55e4332c0395fd1659f13a0eed0
GeovaniWeizenmann <geovaniweizenmann@gmail.com>
2015-04-30 12:44:09 -0300

    Barra de Vida troca de cor de acordo com o tamanho dela.
    Pode testar apertando a tecla 'z'.
    Não coloquei ela em todos os objetos porque não consertei ainda uns bugs
    que podem acontecer nos outros, mas que não aparecem quando usada no
    Guardian.

--- Source code/Mirah's Guardian/Assets/Scripts/Interface/BarraDeVida.cs
+++ Source code/Mirah's Guardian/Assets/Scripts/Interface/BarraDeVida.cs
@@ -1,13 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI; //Nao esquecer de sempre dar esse include.
 
 public class BarraDeVida : MonoBehaviour {
 
-	void Start () {
+	public Image image;
 
+	void Start () {
+		image = GetComponent<Image>();
 	}
 	
 	void Update () {
 		transform.localScale = new Vector3(GetComponentInParent<Character>().healthPoints/3, 1, 0);
+		image.color = Color.Lerp(Color.red, Color.green,Mathf.Lerp(0,1,transform.localScale.x));
 	}
 }
