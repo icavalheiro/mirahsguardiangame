0f0f1e1261c25f68de71043c2da75218e0e6528b
GeovaniWeizenmann <geovaniweizenmann@gmail.com>
2015-07-02 06:23:56 -0300

    Audio no menu, nova sprite do escudo e corrigido um bug na interface in game
    Mas a música dos menus não volta se voltar para o menue o som sempre
    começa mutado.

---
+++ Source code/Mirah's Guardian/Assets/Scripts/Musics.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Musics : MonoBehaviour {
+
+	public AudioSource ostMenu; //Setar na aba Inspector
+	public AudioSource ostInGame;
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(transform.gameObject);
+		if(Application.loadedLevel < 4)
+		{
+			ostMenu.Play();
+			//ostMenu.loop() = true;
+		}
+		else
+		{
+			ostInGame.Play();
+			//ostInGame.loop() = true;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
